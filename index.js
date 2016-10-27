75a8b73a036e3b215bab6240657ef73c8c708dec
bredele <olivier.wietrich@gmail.com>
2016-10-27 12:54:53 -0600

    fulfill promise sychronously

--- index.js
+++ index.js
@@ -8,15 +8,20 @@
  */
 
 module.exports = function promise(resolver) {
+  var state = 'pending'
+  var result
   var fulfilled = []
   resolver(function(value) {
+    state = 'fulfilled'
+    result = value
     fulfilled.map(function(cb) {
       cb(value)
     })
   })
   return {
     then: function(fulfill, reject) {
-      fulfilled.push(fulfill)
+      if(state != 'fulfilled') fulfilled.push(fulfill)
+      else fulfill(result)
     }
   }
 }
