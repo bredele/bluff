49feed3f2db86817505e3cb5411ab7f839460b7d
bredele <olivier.wietrich@gmail.com>
2016-10-27 12:57:39 -0600

    reject promise

--- index.js
+++ index.js
@@ -11,17 +11,27 @@ module.exports = function promise(resolver) {
   var state = 'pending'
   var result
   var fulfilled = []
+  var rejected = []
   resolver(function(value) {
     state = 'fulfilled'
     result = value
     fulfilled.map(function(cb) {
       cb(value)
     })
+  }, function(reason) {
+    state = 'rejected'
+    result = reason
+    rejected.map(function(cb) {
+      cb(reason)
+    })
   })
   return {
     then: function(fulfill, reject) {
-      if(state != 'fulfilled') fulfilled.push(fulfill)
-      else fulfill(result)
+      if(state == 'pending') {
+        fulfilled.push(fulfill)
+        rejected.push(reject)
+      } else if(state == 'fulfilled') fulfill(result)
+      else reject(result)
     }
   }
 }
